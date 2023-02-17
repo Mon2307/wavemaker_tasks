943b6ea025f88c2db0a8baccab4e670391b22009
Monika <monikahamand237@gmail.com>
2023-02-17 14:15:31 +0530

    Sql file added

---
+++ JDBC_Assignment/book_details_202302171407.sql
@@ -0,0 +1,9 @@
+INSERT INTO library.book_details (book_id,name,booktype_id,author_name,publication,language_id,cost,isavailable,total_copies,shelf_name) VALUES
+	 (1,'The book named book',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1001,'The White Tiger',1,' Aravind Adiga','Yashraj',11,500,1,10,'A1'),
+	 (1002,'Looking for Alaska',1,' John Green','Home Publication',11,300,1,5,'A1'),
+	 (1003,'The Summer Is Ended and We Are Not Yet Saved by',2,' Joey Comeau','Home Publication',11,350,1,10,'A2'),
+	 (1004,'The Woman in Black',2,'Susan Hill','Home Publication',11,450,1,20,'A2'),
+	 (1005,'Kaiju Preservation Society',4,' John Scalzi','Home Publication',11,550,1,10,'A4'),
+	 (1006,' Jade Lady Burning',6,' Martin Limon','Home Publication',15,650,1,5,'A6'),
+	 (1012,'The',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
